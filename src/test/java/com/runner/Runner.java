3e34fa8c6bacd61b45d09340fbb098287f046e68
swatisbci <swatisbci@gmail.com>
2022-12-31 20:48:00 -0600

    changes for git practice

--- src/test/java/com/runner/Runner.java
+++ src/test/java/com/runner/Runner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/resources/features",glue ="com.stepDefinations"
-,monochrome=true, tags =("@ds or @linkedlist or @arrays"),
+,monochrome=true, tags =("@ds or @linkedlist"),
 //tags = "@ds", dryRun = false, 
 
 plugin= {"pretty","html:target/cucumber.html"
