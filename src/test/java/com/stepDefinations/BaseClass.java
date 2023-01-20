88f4cdc9609f19315e5b767a96823c0b4100b6e6
balavalli-kamarsu <97117913+balavalli-kamarsu@users.noreply.github.com>
2023-01-20 14:58:33 -0500

    Reverting Changes

--- src/test/java/com/stepDefinations/BaseClass.java
+++ src/test/java/com/stepDefinations/BaseClass.java
@@ -1,21 +1,25 @@
 package com.stepDefinations;
 
-import org.openqa.selenium.WebDriver;
-
-import com.pageObjects.Ds_IntroPage;
+import com.pageObjects.ArraysPage;
+import com.pageObjects.DS_IntroPage;
+import com.pageObjects.GraphPage;
+import com.pageObjects.LinkedListPage;
 import com.pageObjects.LoginPage;
+import com.pageObjects.QueuePage;
 import com.pageObjects.RegisterPage;
+import com.pageObjects.StackPage;
 import com.pageObjects.StartPage;
+import com.pageObjects.TreePage;
 
 public class BaseClass {
 	public 	StartPage sp;
 	public RegisterPage registerPage;
 	public LoginPage loginPage;
-	public Ds_IntroPage ds_IntroPage;
-
-	
-	
-
-		
-
+	public DS_IntroPage ds_IntroPage;
+	public LinkedListPage listPage;
+	public ArraysPage arrayPage;
+	public StackPage stackPage;
+	public GraphPage graphPage;
+	public TreePage treePage;
+	public QueuePage queuePage;
 }
