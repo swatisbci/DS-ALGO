211737ff32c23db1f9481cdccb4ea81da85c0b58
vanisuruvu <76887982+vanisuruvu@users.noreply.github.com>
2023-01-20 15:21:43 -0500

    Reverting changes
    Reverting changes

--- src/test/java/com/runner/Runner.java
+++ src/test/java/com/runner/Runner.java
@@ -3,15 +3,18 @@ package com.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/resources/features",glue ="com.stepDefinations"
-,monochrome=true,dryRun = false, 
-//dry run=true doesnot open the browser
-//tags="@register",
+@CucumberOptions(features="src/test/resources/features"
+,glue ={"com.stepDefinations","com.utils.Helper", "appHooks"}
+,monochrome=true,dryRun = false ,   
+tags = "@ds",
 
-plugin= {"pretty","html:target/cucumber.html"
+plugin= {"pretty","html:target/cucumber.html",
+		"json:target/cucumber-reports/reports.json", 
+		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 })
+
 public class Runner extends AbstractTestNGCucumberTests {
-	
 	
 
 }
