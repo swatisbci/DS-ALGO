4e2a884bb79cdd447cdfa92602b18833d4809905
vanisuruvu <76887982+vanisuruvu@users.noreply.github.com>
2023-01-20 14:56:28 -0500

    Reverting changes
    Reverting changes

--- src/test/java/com/pageObjects/RegisterPage.java
+++ src/test/java/com/pageObjects/RegisterPage.java
@@ -1,18 +1,18 @@
 package com.pageObjects;
 
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.utils.Helper;
+import com.utils.Loggerload;
 import com.utils.Utils;
+
+import junit.framework.Assert;
 
 public class RegisterPage {
 	public RegisterPage(WebDriver webDriver) {
@@ -20,7 +20,6 @@ public class RegisterPage {
 	}
 
 	WebDriver driver = Helper.getDriver();
-	
 	
 	@FindBy(linkText = "Register")
 	@CacheLookup
@@ -58,123 +57,122 @@ public class RegisterPage {
 	@CacheLookup
 	WebElement message;
 	
-	@FindBy(xpath = "//div[contains(text(),'password_mismatch:The two password fields didn’t match.')]")
+	@FindBy(xpath = "//div[@class='alert alert-primary']")
 	@CacheLookup
 	WebElement passwordMismatch;
 	
 	
 	public void clickRegisterLink() throws Exception {
 		PageFactory.initElements(driver, this);
+		Loggerload.info("click on RegisterPage "+registerLink.getText()+" link");
 		Utils.webClick(registerLink);
-		Thread.sleep(2000);
 	}
 	
 	public void sendUsernameText(String username) throws InterruptedException {
 		PageFactory.initElements(driver, this);
-		Utils.webSendKeys(usernameText, username);
-		Thread.sleep(2000);
+		Loggerload.info("sending keys on RegisterPage "+usernameText.getText()+" text field");
+		Utils.webSendKeys(usernameText, Utils.getRandomNum()+username);
 	}
 	
 	public void sendPasswordText1(String password1) throws InterruptedException {
+		Loggerload.info("sending keys on RegisterPage "+passwordText1.getText()+" text field");
 		Utils.webSendKeys(passwordText1, password1);
-		Thread.sleep(2000);
 	}
 	
 	public void sendPasswordText2(String password2) throws Exception {
+		Loggerload.info("sending keys on RegisterPage "+passwordText2.getText()+" text field");
 		Utils.webSendKeys(passwordText2, password2);
-		Thread.sleep(2000);
 	}
 	
 	public void clickRegisterButton() throws Exception {
+		Loggerload.info("click on RegisterPage "+registerButton.getText()+" button");
 		Utils.webClick(registerButton);
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 	}
 	
 	public String getNewAccountCreatedLabelText() {
+		Loggerload.info("validating on RegisterPage "+newAccountCreatedLabel.getText()+" label text");
 		return newAccountCreatedLabel.getText();
-	
 	}
 	
 	public void clickSignOutLink() throws Exception {
+		Loggerload.info("click on RegisterPage "+signOutLink.getText()+" link");
 		Utils.webClick(signOutLink);
 		Thread.sleep(2000);
-		
 	}
 	
 	public String validationEmptyFields() throws InterruptedException {
-		//Thread.sleep(3000);
+		Loggerload.info("validating on RegisterPage "+usernameText.getText()+" text field");
 		String mesg=usernameText.getAttribute("validationMessage");
-		System.out.println(mesg);
 		return mesg;
 	}
 	public String validationEmptyFieldspassword1() throws InterruptedException {
-		//Thread.sleep(3000);
+		Loggerload.info("validating on RegisterPage "+passwordText1.getText()+" text field");
 		String mesg=passwordText1.getAttribute("validationMessage");
-		System.out.println(mesg);
 		return mesg;
 	}
 	public String validationEmptyFieldspassword2() throws InterruptedException {
-		//Thread.sleep(3000);
+		Loggerload.info("validating on RegisterPage "+passwordText2.getText()+" text field");
 		String mesg=passwordText2.getAttribute("validationMessage");
-		System.out.println(mesg);
 		return mesg;
 	}
 	
-	public String validationPwdMismatch() throws InterruptedException {
-		//Thread.sleep(3000);
-		
-		String pwd1=passwordText1.getText();
-		String pwd2=passwordText2.getText();
-		String msg = "";
-		if(pwd1 != pwd2) {
-			 msg=passwordMismatch.getText();
-			
+	@SuppressWarnings("deprecation")
+	public void validatePasswordMismatchInputs(String pwd1, String pwd2) {
+		if(pwd1.equals(pwd2)) {
+			Assert.fail();
 		}
+	}
+	
+	public String validationPwdMismatch() throws InterruptedException {
+		String msg = passwordMismatch.getText();
 		return msg;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void validateLessThanEightChars(String pwd1, String pwd2) {
+		// checking if length of passwords is less than 8, if not fail the test case
+		if(!(pwd1.length() < 8 || pwd2.length() < 8)) {
+			Assert.fail();
+		}
 	}
 	
 	public String validatepwd() throws InterruptedException {
-		//Thread.sleep(3000);
-		String pwd1=passwordText1.getText();
-		int count = 0;
-		String msg = "";
-		 for(int i = 0; i < pwd1.length(); i++) {    
-	            if(pwd1.charAt(i) != ' ')    
-	                count++;    
-	        }    
-		if(count < 8) {
-				msg = passwordMismatch.getText();
-			
-		}
+		String msg = passwordMismatch.getText();
 		return msg;
-	}
-	public String validatecompare() throws InterruptedException {
-		//Thread.sleep(3000);
-		String msg = "";
-		String usr=usernameText.getText();
-		String pwd1=passwordText1.getText();
-		String pwd2=passwordText2.getText();
-		boolean a=(usr==pwd1);
-	    boolean b =(usr==pwd2);
-		if(a&b==true) {
-			msg = passwordMismatch.getText();
-		}
-		return msg;
-	}
-	public String validatenumeric() throws InterruptedException {
-		//Thread.sleep(3000);
-		String msg = "";
-		String pwd1=passwordText1.getText();
-		 boolean a = (PasswordIsDigits(pwd1));
-	       if(a==true) {
-	    	   msg = passwordMismatch.getText();
-	       }
-		return msg;}
-
-		private static boolean PasswordIsDigits(String password) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
 	}
 	
+	public void validateCompareUnPwd(String username, String pwd1, String pwd2) {
+		if(!(username.equals(pwd1) && username.equals(pwd2))) {
+			Assert.fail(); 
+			// failing test case when username, pwd1, pwd2 are not same
+		}
+	}
+	
+	public String validatecompare() throws InterruptedException {
+		//Thread.sleep(3000);
+//		String msg = "";
+//		String usr=usernameText.getText();
+//		String pwd1=passwordText1.getText();
+//		String pwd2=passwordText2.getText();
+//		boolean a=(usr==pwd1);
+//	    boolean b =(usr==pwd2);
+//		if(a&b) {
+//			msg = passwordMismatch.getText();
+//		}
+		return passwordMismatch.getText();
+	}
+	
+	
+	public void validateNumericInputs(String pwd1, String pwd2) {
+		if(!(StringUtils.isNumeric(pwd1) && StringUtils.isNumeric(pwd2))) {
+			// if passwords are not numeric, then fail the test case
+			Assert.fail();
+		}
+	}
+	
+	public String validatenumeric() {
+	    return passwordMismatch.getText();
+	}
+
+}
