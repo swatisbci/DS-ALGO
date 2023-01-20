ad13c6a058663c841dad2a9da545ea67db7fd1ac
balavalli-kamarsu <97117913+balavalli-kamarsu@users.noreply.github.com>
2023-01-20 18:41:19 -0500

    Updating changes from master

--- src/test/java/com/stepDefinations/StackStepDef.java
+++ src/test/java/com/stepDefinations/StackStepDef.java
@@ -3,13 +3,13 @@ package com.stepDefinations;
 import static org.testng.Assert.assertEquals;
 
 import java.util.List;
-
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
 import com.pageObjects.LinkedListPage;
 import com.pageObjects.StackPage;
 import com.utils.Helper;
+
 import com.utils.Loggerload;
 
 import io.cucumber.java.en.Given;
@@ -18,18 +18,12 @@ import io.cucumber.java.en.When;
 
 public class StackStepDef extends BaseClass {
 	WebDriver driver = Helper.getDriver();
-	
 
 @When("user click on Get Started button in Stack section")
 public void user_click_on_get_started_button_in_stack_section() {
 	stackPage = new StackPage(driver);
     stackPage.clickGetStarted();
 }
-//@Then("user should navigate to corresponding stack page with title {string}")
-//public void user_should_navigate_to_corresponding_stack_page_with_title(String title) {
-//	Assert.assertEquals(title, Helper.getDriver().getTitle());
-//    
-//}
 
 @When("user click on operations in stack link")
 public void user_click_on_operations_in_stack_link() {
@@ -66,12 +60,18 @@ public void user_enters_invalid_python_code_in_text_editor(io.cucumber.datatable
 	 List<List<String>>data=pythonCode.cells();
 	 listPage.enterCode(data.get(0).get(0)); 
 }
+
 @Then("user should get error message")
 public void user_should_get_error_message() {
 	stackPage = new StackPage(Helper.getDriver());
 	String actualMsg = stackPage.getErrormsg();
 	Loggerload.info("Actual result  :" + actualMsg);
 	assertEquals(actualMsg,"NameError: name 'hello' is not defined on line 1", "Result do not match");
+}
+
+@Then("user should navigate to corresponding stack page with title {string}")
+public void user_should_navigate_to_corresponding_stack_page_with_title(String title) {
+	Assert.assertEquals(title, Helper.getDriver().getTitle());
 }
 
 }
